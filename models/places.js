65b9fa4f32f6706d1ad485f03fcf6efd36207371
Brad Evans <kaloba4r@gmail.com>
2019-08-18 01:09:34 -0400

    added static data for places to align with master data already in place

--- models/places.js
+++ models/places.js
@@ -4,8 +4,23 @@ module.exports = function(sequelize, DataTypes) {
     image_url: DataTypes.STRING,
     wifi: DataTypes.BOOLEAN,
     pet_friendly: DataTypes.BOOLEAN,
-    large_groups: DataTypes.BOOLEAN
+    bigtables: DataTypes.BOOLEAN,
+    neighborhood: DataTypes.STRING,
+    address: DataTypes.STRING,
+    waiters: DataTypes.BOOLEAN,
+    kidfriendly: DataTypes.BOOLEAN,
+    website: DataTypes.STRING
   }
   );
+  
+  Place.associate = function(models) {
+    // assoiates a place with its reviews
+    Place.hasMany(models.Review, {
+      onDelete: "cascade"
+    });
+  };
+
   return Places;
 };
+
+
