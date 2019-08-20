36081459e6b3a1af6befbf86589d33fd92864c58
Zoe Dj <studio3fun@gmail.com>
2019-08-19 20:51:28 -0400

    "html"

--- public/js/places.js
+++ public/js/places.js
@@ -1,32 +1,80 @@
-// Get references to page elements
-var $submitBtn = $(".submit-review");
+$(document).ready(function() {
 
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveReview: function(review) {
-    return $.ajax({
-      review: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/reviews",
-      data: JSON.stringify(review)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
+  
+  
+  
+   /// Placeholder for Places 
+  
+    // Event Handler Functions
+  function getPlaces(){
+    console.log('CLIENT-SIDE SEND GET REQUEST')
+    $.get("/api/places", function(data){
+      for (var i =0; i<data.length; i++){
+        var row = $("<div>");
+        $row.addClass("place");
+        $row.append (
+          "<p>" + data[i].neighborhood + "</p>",
+          "<p>" + data[i].address + "</p>",
+          "<img href=' " + data[i].photo + " ' alt='cool place to study'> ",
+          "<p>" + data[i].wifi + "</p>",
+          "<p>" + data[i].waiters + "</p>",
+          "<p>" + data[i].kidfriendly + "</p>",
+          "<p>" + data[i].noiselevel + "</p>",
+          "<p>" + data[i].website + "</p>",
+        );
+    }
+    console.log(place)
+    
     });
   }
-};
+  
+    // Run Functions
+    getPlaces();
+  
+  });
+
+
+  // neighborhood: 
+  // address: “5126 Park Rd #1d, Charlotte, NC 28209”
+  // photo: “https://images.app.goo.gl/64aLsAWdhD3WWawy6”
+  // wifi: true
+  // waiters: true
+  // dogfriendly:
+  // kidfriendly: true
+  // website: “http://mugsofcharlotte.com/“
+  
+
+
+// // Get references to page elements
+// var $submitBtn = $(".submit-review");
+
+
+// // The API object contains methods for each kind of request we'll make
+// var API = {
+//   saveReview: function(review) {
+//     return $.ajax({
+//       review: {
+//         "Content-Type": "application/json"
+//       },
+//       type: "POST",
+//       url: "api/reviews",
+//       data: JSON.stringify(review)
+//     });
+//   },
+//   getExamples: function() {
+//     return $.ajax({
+//       url: "api/examples",
+//       type: "GET"
+//     });
+//   },
+//   deleteExample: function(id) {
+//     return $.ajax({
+//       url: "api/examples/" + id,
+//       type: "DELETE"
+//     });
+//   }
+// };
+
 
 // // refreshExamples gets new examples from the db and repopulates the list
 // var refreshExamples = function() {
