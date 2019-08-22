8d181427d6a347249588b6e8aab0abfd165ab9f7
Zoe Djukic <zoedjukic@Zoes-MacBook-Air.local>
2019-08-21 20:34:20 -0400

    places

--- models/seeds_reviews.sql
+++ models/seeds_reviews.sql
@@ -17,8 +17,9 @@
 
 INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Ellen Delaney','Brix Uptown', 'Foodie haven it is not, but great space for studying', 4, NOW(), NOW(),1);
 INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Omrie Johnson','Coco and the Director', 'You will have a hard time believing you have not been whisked away to Paris!', 5, NOW(), NOW(),3);
-INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Jason B','Seeds Mugs Coffee', 'Delicious pastry and a fabulous latte', 4, NOW(), NOW(),7);
+INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Jason B','Mugs Coffee', 'Mugs is one of my favorite study spots in Charlotte. Cozy atmosphere with plenty of tables to get work done.  Plenty of parking in the front.', 4, NOW(), NOW(),7);
 INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Celeste Duprie','Huntersville Waterbean Coffee', ' Good for large groups.  There was a large party seated downstairs, and our party of 3 was seated upstairs.', 3, NOW(), NOW(),6);
-INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Sully Washington','Whole Foods South CLT', '“The wine/beer bar (on second floor) & wine/lounge area above is so nice.', 4, NOW(), NOW(),9);
-INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Caitlin Steadman','Queen City Tea Bar', 'They have an extensive selection of teas and drinks that can appeal to most if not all palates. I’m a big fan of oolong tea so when I saw that there is a roasted oolong milk tea, I was sold for life!', 4, NOW(), NOW(),5);
+INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Sully Washington','Whole Foods South CLT', 'Pros: big community tables, coffee/pizza/store on the bottom floor, bathrooms on the same level, outdoor patio, closed room that you can reserve.', 4, NOW(), NOW(),9);
+INSERT INTO reviews (user_name, place_name, content, rating,createdAt, updatedAt, placeId) VALUES ('Nick Held','The Olde Mecklenburg Brewery', 'A huge outdoor seating area is a perfect meeting spot on a nice fall or spring day. We come to study there for hours and enjoy some beer and quick bits afterwards.', 4, NOW(), NOW(),8);
+
 
--- public/js/places.js
+++ public/js/places.js
@@ -1,112 +1,44 @@
 $(document).ready(function () {
-  var displayPlaces = $("#display-places");
-  var placeHoodSelect = $("#neiborhood");
 
   // Adding event listeners to the form to create a place object
-  $(document).on(".submit", "#place-form", handlePlaceFormSubmit);
+  $("#submit").on("click", function(event){
+  event.preventDefault();
 
-  // Getting the initial list of places
-  getPlaces();
-
-
-  // A function to handle what happens when the form is submitted to create a new place
-  function handlePlaceFormSubmit(event) {
-    event.preventDefault();
-    
-  // Don't do anything if the name fields hasn't been filled out
-    if (!nameInput.val().trim()
-
-    //   add other feilds
-
-    ) {
-      
-      return;
+    // This grabs values from user input
+    var newPlace = {
+    place_name: $("#inputNameMD").val().trim(),
+    // address_name: $("#inputAddressMD").val().trim(),
+    address_name: $("#inputZipMD").val().trim(),
+    wifi: $("#inputWifi:checked").val(),
+    waiters: $("#inputWaiters:checked").val(),
+    kidfriendly: $("#inputKidfriendly:checked").val(),
+    bigtables: $("#inputBigtables:checked").val(),
+    pet_friendly: $("#inputPetfriendly:checked").val(),
+    website: $("#inputWebsite").val().trim()
     }
-    // Calling the newPlace function and passing in the value of the inputs
-    newPlace({
-      place_name: nameInput.val().trim(),
-      address: addressInput.val().trim(),
-      neighborhood: neighborhoodInput.val().trim(),
-      wifi: wifiInput.val(),
-      waiters: waitersInput.val(),
-      kidfriendly: kidfriendlyInput.val(),
-      bigtables: bigtablesInput.val(),
-      dogfriendly: petfriendlyInput.val(),
-      website: websiteInput.val().trim()
-    });
-  }
 
-  // A function for creating a place. Calls getPlaces upon completion
-  function newPlace(placeData) {
-    $.post("/api/places", placeData)
-      .then(getPlaces);
-  }
+    // This clears all of the text boxes
+    $("#inputNameMD").val("");
+    $("#inputAddressMD").val("");
+    $("#inputZipMD").val("");
+    $("#inputWifi:unchecked").val("");
+    $("#inputWaiters:unchecked").val("");
+    $("#inputKidfriendly:unchecked").val("");
+    $("#inputBigtables:unchecked").val("");
+    $("#inputPetfriendly:unchecked").val("");
+    $("#inputWebsite").val("");
 
-  // Function for creating a new list row for places
-  function createPlaceRow(placeData) {
-    var row = $("<div>");
-    row.data("place", placeData);
-    row.append("<p>" + placeData.place_name + "</p>");
-    row.append("<p>" + placeData.address + "</p>");
-    row.append("<img src= '" + placeData.image_url + "'>");
-    row.append("<p>" + placeData.wifi + "</p>");
-    row.append("<p>" + placeData.waiters + "</p>");
-    row.append("<p>" + placeData.kidfriendly + "</p>");
-    row.append("<p>" + placeData.pet_friendly + "</p>");
-    row.append("<p>" + placeData.bigtables + "</p>");
-    row.append("<p>" + placeData.website + "</p>");
-    return row;
-  }
+    makeNewPlace(newPlace)
 
-
-  // Function for retrieving places and getting them ready to be rendered to the page
-  function getPlaces() {
-    $.get("/api/places", function(data) {
-      var rowsToAdd = [];
-      for (var i = 0; i < data.length; i++) {
-        rowsToAdd.push(createPlaceRow(data[i]));
-      }
-      renderPlaceList(rowsToAdd);
-      nameInput.val("");
-      addressInput.val("");
-      wifiInput.val("");
-      waitersInput.val("");
-      kidfriendlyInput.val("");
-      bigtablesInput.val("");
-      dogfriendlyInput.val("");
-      websiteInput.val("");
-    });
-  }
-
-  // // A function for rendering the list of places to the page
-  // function renderPlaceList(rows) {
-  //   placeList.children().not(":last").remove();
-  //   displayPlaces.children(".alert").remove();
-  //   if (rows.length) {
-  //     console.log(rows);
-  //     placeList.prepend(rows);
-  //   }
-  //   else {
-  //     renderEmpty();
-  //   }
-  // }
-
-  // // Function for handling what to render when there are no authors
-  // function renderEmpty() {
-  //   var alertDiv = $("<div>");
-  //   alertDiv.addClass("alert alert-danger");
-  //   alertDiv.text("You must create an Author before you can create a Post.");
-  //   authorContainer.append(alertDiv);
-  // }
-
-  // // Function for handling what happens when the delete button is pressed
-  // function handleDeleteButtonPress() {
-  //   var listItemData = $(this).parent("td").parent("tr").data("author");
-  //   var id = listItemData.id;
-  //   $.ajax({
-  //     method: "DELETE",
-  //     url: "/api/authors/" + id
-  //   })
-  //     .then(getAuthors);
-  // }
 });
+
+
+  // A function for creating a new study place.
+  function makeNewPlace(placeData) {
+    $.post("/api/places", placeData)
+  }
+
+});
+
+// Don't do anything if the name fields hasn't been filled out
+// if (!nameInput.val().trim()
